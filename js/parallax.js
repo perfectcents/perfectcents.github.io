69ae6b8760eb7d0733ce747736921ba8b8712d7b
shsalt <shepards.alt@gmail.com>
2015-02-24 19:29:48 -0800

    pls

---
+++ js/parallax.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+$objWindow = $(window);
+$('div[data-type="background"]').each(function(){
+var $bgObj = $(this);
+$(window).scroll(function() {
+var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+var coords = '100% '+ yPos + 'px';
+// Change the position of background
+$bgObj.css({ backgroundPosition: coords });
+ 
+});
+ 
+});
+});
